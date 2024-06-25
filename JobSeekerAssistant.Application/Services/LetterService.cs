2f7410df0f7d396aacc9acd93f9cd7203f20eea7
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-06-25 18:44:41 +0200

    implemented letterService

---
+++ JobSeekerAssistant.Application/Services/LetterService.cs
@@ -0,0 +1,37 @@
+﻿using JobSeekerAssistant.Application.Interfaces.Repositories;
+using JobSeekerAssistant.Application.Interfaces.Services;
+using JobSeekerAssistant.Domain.Entities;
+
+namespace JobSeekerAssistant.Application.Services;
+
+public class LetterService(ILetterRepository<string> letterRepository) : ILetterService<string>
+{
+    private readonly ILetterRepository<string> _letterRepository = letterRepository;
+    public async Task<IEnumerable<Letter>> GetAllByUserIdAsync(string userId)
+    {
+        var letters = await _letterRepository.GetAllByUserIdAsync(userId);
+        return letters;
+    }
+
+    public async Task<Letter> GetByIdAsync(string id)
+    {
+        var letter = await _letterRepository.GetByIdAsync(id);
+
+        return letter;
+    }
+
+    public async Task AddAsync(Letter entity)
+    {
+        await _letterRepository.AddAsync(entity);
+    }
+
+    public async Task UpdateAsync(Letter entity, string id)
+    {
+        await _letterRepository.UpdateAsync(entity, id);
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        await _letterRepository.DeleteAsync(id);
+    }
+}
