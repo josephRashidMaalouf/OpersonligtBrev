f87d70097f42501f626cb7fc6a298504feda8402
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-06-25 15:13:37 +0200

    Created migration

--- JobSeekerAssistant.Api/Program.cs
+++ JobSeekerAssistant.Api/Program.cs
@@ -1,6 +1,7 @@
 using JobSeekerAssistant.Domain.Entities.Identity;
 using JobSeekerAssistant.Infrastructure;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
