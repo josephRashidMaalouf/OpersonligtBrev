3b18b098221a233cce8b2c018a9405e2812109be
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-06-25 12:05:44 +0200

    Added JobDto

---
+++ JobSeekerAssistant.Domain/Dtos/JobDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JobSeekerAssistant.Domain.Dtos;
+
+public class JobDto
+{
+    public string CompanyName { get; set; }
+    public string About { get; set; }
+    public string JobDescription { get; set; }
+}
