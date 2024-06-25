5a9c4f9610edbc4487328bb5d8b3eda5bea2d05c
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-06-25 15:10:26 +0200

    Installed nugets for sql server

--- JobSeekerAssistant.Api/Program.cs
+++ JobSeekerAssistant.Api/Program.cs
@@ -1,3 +1,7 @@
+using JobSeekerAssistant.Domain.Entities.Identity;
+using JobSeekerAssistant.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme).AddIdentityCookies();
+builder.Services.AddAuthorizationBuilder();
+
+var sqlConnectionString = builder.Configuration["Database:Local:Sql"];
+var mongoConnectionString = builder.Configuration["Database:Local:Mongo"];
+var gptKey = builder.Configuration["GptKey"];
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(sqlConnectionString);
+});
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
 
 var app = builder.Build();
 
