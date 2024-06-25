45e8f0e24a94351fbe68d96bb234f40ea8e39708
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-06-25 14:44:16 +0200

    Added a dbcontext

---
+++ JobSeekerAssistant.Infrastructure/AppDbContext.cs
@@ -0,0 +1,51 @@
+﻿using JobSeekerAssistant.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobSeekerAssistant.Infrastructure;
+
+public class AppDbContext(DbContextOptions options) : IdentityDbContext<User, Role, string,
+    IdentityUserClaim<string>, UserRole, IdentityUserLogin<string>,
+    IdentityRoleClaim<string>, IdentityUserToken<string>>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<User>(b =>
+        {
+            // Each User can have many UserClaims
+            b.HasMany(e => e.Claims)
+                .WithOne()
+                .HasForeignKey(uc => uc.UserId)
+                .IsRequired();
+
+            // Each User can have many UserLogins
+            b.HasMany(e => e.Logins)
+                .WithOne()
+                .HasForeignKey(ul => ul.UserId)
+                .IsRequired();
+
+            // Each User can have many UserTokens
+            b.HasMany(e => e.Tokens)
+                .WithOne()
+                .HasForeignKey(ut => ut.UserId)
+                .IsRequired();
+
+            b.HasMany(e => e.UserRoles)
+                .WithOne(e => e.User)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+        });
+
+
+        builder.Entity<Role>(b =>
+            b.HasMany(e => e.UserRoles)
+                .WithOne(e => e.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired()
+        );
+    }
+
+}
