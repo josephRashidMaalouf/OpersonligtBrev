3ca4c683c22ae0fcee0330f6afadf58b5307e388
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-07-01 11:09:03 +0200

    Http client in program.cs

--- Frontend/JobSeekerAssistant.Client.BlazorClient/Program.cs
+++ Frontend/JobSeekerAssistant.Client.BlazorClient/Program.cs
@@ -1,4 +1,7 @@
 using JobSeekerAssistant.Client.BlazorClient;
+using JobSeekerAssistant.Client.BlazorClient.Identity;
+using JobSeekerAssistant.Client.Domain.Interfaces;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,20 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddTransient<CookieHandler>();
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+
+builder.Services.AddScoped(
+    sp => (IAccountManagement)sp.GetRequiredService<AuthenticationStateProvider>());
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient(
+        "JobSeekerAssistantApi",
+        opt => opt.BaseAddress = new Uri("https://localhost:7137"))
+    .AddHttpMessageHandler<CookieHandler>();
 
 await builder.Build().RunAsync();
