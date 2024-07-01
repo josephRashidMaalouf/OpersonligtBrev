17bb062239545c4c6a9910f1b469c06068e604a8
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-07-01 22:20:34 +0200

    Wrote an interface for client services

---
+++ Frontend/JobSeekerAssistant.Client.Application/Interfaces/IService.cs
@@ -0,0 +1,10 @@
+﻿using JobSeekerAssistant.Client.Domain.Interfaces;
+
+namespace JobSeekerAssistant.Client.Application.Interfaces;
+
+public interface IService<TDto, TId> where TDto : IDto<TId>
+{
+    public Task<IEnumerable<TDto>> GetAllByUserIdAsync(string userId);
+    public Task<bool> UpdateAsync(string userId);
+    public Task<bool> DeleteAsync(string userId);
+}
---
+++ Frontend/JobSeekerAssistant.Client.Domain/Interfaces/IDto.cs
@@ -0,0 +1,6 @@
+﻿namespace JobSeekerAssistant.Client.Domain.Interfaces;
+
+public interface IDto<TId>
+{
+    public TId Id { get; set; }
+}
