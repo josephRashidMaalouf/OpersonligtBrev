3dc90a45485f3091b6846bd6465d3c0bf6209fd1
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-07-14 20:22:39 +0200

    Preparations

--- Frontend/JobSeekerAssistant.Client.Domain/Models/LetterModel.cs
+++ Frontend/JobSeekerAssistant.Client.Domain/Models/LetterModel.cs
@@ -4,9 +4,8 @@ namespace JobSeekerAssistant.Client.Domain.Models;
 
 public class LetterModel : IModel<string>
 {
-    public string Id { get; set; }
-    public string Title { get; set; }
-    public string Text { get; set; }
-    public string UserId { get; set; }
-    public string UserEmail { get; set; }
+    public string Id { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public string UserEmail { get; set; } = string.Empty;
 }
